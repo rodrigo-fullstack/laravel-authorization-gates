c2cb3379f85ce5a0b57e54f09f8ad7ce87d5e159
rodrigo-backend <rodrigopereirfaria9@gmail.com>
2025-03-11 11:57:35 -0300

    feat(auth): authorize allowed users to see the buttons to update and delete

--- resources/views/components/product-row.blade.php
+++ resources/views/components/product-row.blade.php
@@ -3,7 +3,19 @@
     <td>{{ $product['name'] }}</td>
     <td>{{ $product['value'] }}</td>
     <td>
-        <a href="#">UPDATE</a>
-        <a href="#">DELETE</a>
+        @can('user_admin')
+            <a href="{{ route('product.update', ['data' => http_build_query($product)]) }}">UPDATE</a>
+        @endcan
+
+        @can('user_admin')
+            <form action="{{ route('product.delete.submit') }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <input type="hidden" name="id" id="id" value="{{ $product['id'] }}">
+                <button type="submit">DELETE</button>
+
+            </form>
+            
+        @endcan
     </td>
 </tr>
