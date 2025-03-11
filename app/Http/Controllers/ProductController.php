cf5d536238bfe2aa08e8cdbfe7f14b3b60d3fd75
rodrigo-backend <rodrigopereirfaria9@gmail.com>
2025-03-11 11:58:17 -0300

    feat(session): change session data based on last operation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\States\From;
 use App\Models\Product;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
@@ -10,41 +12,125 @@ use Illuminate\Support\Facades\Gate;
 class ProductController extends Controller
 {
     public function index(string $data = null){
+        // dd(session()->get('products'));
+        if(session()->exists('success')){
+            session()->remove('success');
+
+        }
         $products = session()->get('products');
+        // dd($products);
 
         // if there is not products in the session, attribute them
         if(!$products){
             // dd("Produtos não está definido na sessão...");
             $products = Product::all()->toArray();
             session()->put('products', $products);
-        
+            // dd($products);
+            return view('dashboard')->with('products', session()->get('products'));
         }
+
+        // dd($data);
 
         // if there was a last update in table
         if($data !== null){
+            // dd($data);
             $result = '';
             parse_str($data, $result);
-            $data = $result['data'];
+            $data = $result;
+            // dd($data['success']);
             // receive the data
             
-            // if there was success update the session products with last product and data is present
+            // if there was success and data is present
             if($data['success']){
                 
-                $product = $data['product'];
-                $products[] = $product;
+                // update the session products with last product 
+                function changeSessionByFrom($products, $data){
+                    // dd($product['from']);
+                    // dd($product['from']['name']);
+                    switch($data['from']['name']){
+                        case From::Create->name: 
+                            $products[] = $data['product']; 
+                            break;
+                        case From::Update->name: 
+                            function updateProductFromSession($products, $data){
+                                for($i = 0; $i < count($products); $i++){
+                                    if(array_key_exists($i, $products)){
+                                        // dd($products[$i]['id']);
+                                        // dd(array_key_exists($i, $products));
+                                        // dd($i);
+                                        // dd("{$products[$i]['id']} = {$product['id']}",
+                                        
+                                        // $products[$i]['id'] === $product['id']);
+                                        if($products[$i]['id'] === (int) $data['id']){
+                                            
+                                            $products[$i] = $data['product'];
+                                            // dd($products);
+                                            return $products;
+                                        }
+                                    }
+                                }
+                            }
+                            updateProductFromSession($products, $data);
+                            break;
+                        case From::Delete->name: 
+                            // dd($products);
+                            function removeProductFromSession($products, $data){
+                                for($i = 0; $i < count($products); $i++){
+                                    if(array_key_exists($i, $products)){
+                                        // dd($products[$i]['id']);
+                                        // dd(array_key_exists($i, $products));
+                                        // dd($i);
+                                        // dd("{$products[$i]['id']} = {$product['id']}",
+                                        
+                                        // $products[$i]['id'] === $product['id']);
+                                        if($products[$i]['id'] === (int) $data['id']){
+                                            // dd($products);
+                                            unset($products[$i]);
+                                            // dd($products);
+                                            return $products;
+                                        }
+                                    }
+                                }
+                                return $products;
+                            }
+
+                            return removeProductFromSession($products, $data);
+                        default: 
+                            throw new Exception("From variable doesn't exist");
+
+                    };
+
+                    return $products;
+                    // if its update
+                    // for($i = Product::first('id')->id; $i < count($products); $i++){
+                    //     // change the products of the session
+                    //     if($i === $product['id'] - 1){
+                    //         $products[$i] = $product;
+                    //         return $products;
+                    //     }
+                    // }
+
+                    // if its not insert the product in the session
+                    ;
+                    // return $products;
+                };
+
+                $products = changeSessionByFrom($products, $data);
+                // dd($products);
+                // dd($products);
                 session()->put('products', $products);
                 session()->put('success', $data['message']);
+                // dd('Session Products: ', session()->get('products'));
 
                 unset($products);
             }
         }
+        
+        // $successMessage = session()->get('success');
+        // !isset($successMessage) ?: session()->remove('success');
+        // dd('Session Products: ', session()->get('products'));
 
-
-        // dd('Produtos definido na sessão...');
-        return gettype($data) === 'null' ? 
-        view('dashboard')->with('products', session()->get('products')) :
-        view('dashboard')->with('products', session()->get('products'));
-        ;
+        return view('dashboard')->with('products', session()->get('products'));
 
     }
 
@@ -91,12 +177,10 @@ class ProductController extends Controller
         
         // return to the view of dashboard
         return redirect()->route('dashboard', http_build_query([
-            'data' => [
-                'success' => true,
-                'message' => 'Product created successfully',
+            'success' => true,
+            'message' => 'Product created successfully',
             'from' => From::Create,
-                'product' => $product->toArray()
-            ]
+            'product' => $product->toArray()
         ]));
     }
 
@@ -107,6 +191,7 @@ class ProductController extends Controller
         
         return $this->$property;
     }
+    
     public function change(?string $data = null){
         // -----------------------
         // from Auth Facades
