de9b2d79f5a63a34bc3a611b82f661ee226d2498
rodrigo-backend <rodrigopereirfaria9@gmail.com>
2025-03-11 11:54:58 -0300

    feat(component): Add the form component data as from and product data

--- app/View/Components/ProductForm.php
+++ app/View/Components/ProductForm.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\View\Components;
 
 use Closure;
@@ -11,15 +10,15 @@ class ProductForm extends Component
     /**
      * Create a new component instance.
      */
-    public function __construct()
-    {
-        //
-    }
+    public function __construct(
+        public string $from,
+        public ?array $product = null
+    ) {}
 
     /**
      * Get the view / contents that represent the component.
      */
-    public function render(): View|Closure|string
+    public function render(): View | Closure | string
     {
         return view('components.product-form');
     }
